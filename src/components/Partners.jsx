74dae7911f8224b59064a94f6769dd14738ab0ae
cottonSdtNadim <nadim.mahmud@cottongroup.com.bd>
2025-03-09 10:16:57 +0600

    Our Partners animation

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,24 +1,79 @@
-import SectionTitle from './Sectiontitle';
-import aldi from '../assets/images/partners-logo/aldi.png';
+import { motion } from "framer-motion";
+import aldi from "../assets/images/partners-logo/aldi.png";
 import auchan from "../assets/images/partners-logo/auchan.png";
+import bestSeller from "../assets/images/partners-logo/bestseller.png";
+import caLayer from "../assets/images/partners-logo/CA_layer.png";
+import carreFour from "../assets/images/partners-logo/carrefour.png";
+import eciLogo from "../assets/images/partners-logo/eci-logo.png";
+import kiabiLondon from "../assets/images/partners-logo/kiabi-london.png";
+import layoutSetLogo from "../assets/images/partners-logo/layout-set-logo.jpg";
+import logoDaRedoute from "../assets/images/partners-logo/logo-da-redoute.png";
+import newYorker from "../assets/images/partners-logo/newyorker.png";
+import next from "../assets/images/partners-logo/next.jpg";
+import nkd from "../assets/images/partners-logo/nkd.png";
+import teamAthletics from "../assets/images/partners-logo/team-athletics.png";
+import topPNG from "../assets/images/partners-logo/top-png.png";
+import trueWorths from "../assets/images/partners-logo/true-worth.jpg";
+import verbaudets from "../assets/images/partners-logo/verbaudet.png";
+import vfAsia from "../assets/images/partners-logo/vf-asia.png";
+import SectionTitle from "./Sectiontitle";
 
-const partners = [aldi, auchan]
+const partners = [
+  aldi,
+  auchan,
+  bestSeller,
+  caLayer,
+  carreFour,
+  eciLogo,
+  kiabiLondon,
+  layoutSetLogo,
+  logoDaRedoute,
+  newYorker,
+  next,
+  nkd,
+  teamAthletics,
+  topPNG,
+  trueWorths,
+  verbaudets,
+  vfAsia,
+];
 
 const Partners = () => {
-    return (
-        <div className='mt-32'>
-            <SectionTitle title={"Our Partners"}></SectionTitle>
+  return (
+    <div className="mt-32">
+      <SectionTitle title={"Our Partners"}></SectionTitle>
 
-            {/* Looping partners array with map() */}
-            <div>
-                {
-                    partners.map(partner => <>
-                    <img src={partner} alt='' className='w-[150px] h-[70px]'  />    
-                    </>)
-                }
+      <div className="relative w-full overflow-hidden">
+        <motion.div
+          className="flex gap-10 mb-8"
+          animate={{ x: [0, -2000] }}
+        // animate={{ x: [-2000, 0, -2000] }}
+          transition={{
+            x: {
+              repeat: Infinity,
+              repeatType: "loop",
+              duration: 10,
+            //   ease: "easeInOut",
+            ease: "linear",
+            },
+          }}
+        >
+          {/* Display two sets of logos to ensure continuous flow */}
+          {[...partners, ...partners].map((partner, index) => (
+            <div key={index} className="w-40 h-16 flex-shrink-0 mx-16">
+              <img
+                src={partner}
+                alt={partner}
+                className="w-full h-full object-contain"
+              />
             </div>
-        </div>
-    );
+          ))}
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default Partners;
+
+
