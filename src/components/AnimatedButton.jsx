d9828aab97075d3c5a18a3f8391c634b7eff690b
cottonSdtNadim <nadim.mahmud@cottongroup.com.bd>
2025-03-10 15:01:21 +0600

    last commit end day

--- src/components/AnimatedButton.jsx
+++ src/components/AnimatedButton.jsx
@@ -13,6 +13,8 @@ const AnimatedButton = ({ label, labelColor, bgColor }) => {
     color: labelColor,
     backgroundColor: bgColor,
     paddingRight: isHovered ? "20px" : "0px", // Dynamic padding based on hover state
+    // transform: isHovered ? "scale(1.1)" : "scale(1)",
+    // transformOrigin: "left", // Set the scaling origin to the left side
 
   }
 
@@ -23,12 +25,14 @@ const AnimatedButton = ({ label, labelColor, bgColor }) => {
   }
 
   return (
-    <div className="">
+    <div>
       <motion.button
         className="px-5 py-2 rounded-lg transition-all duration-300 ease-in-out flex items-center justify-center text-[16px] font-semibold"
         style={buttonStyle}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
+        // whileHover={{ scaleX: 1.1 }} // Increase button size on hover
+        // animate={{ scaleX: isHovered ? 1.1 : 1 }} // Scale only in the horizontal direction
       >
         {label}
         <motion.span
