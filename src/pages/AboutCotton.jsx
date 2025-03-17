95a7e6c97fb321cde0edde3ac8467f5aaffcf01b
cottonSdtNadim <nadim.mahmud@cottongroup.com.bd>
2025-03-17 14:45:00 +0600

    last commit EOD

--- src/pages/AboutCotton.jsx
+++ src/pages/AboutCotton.jsx
@@ -2,6 +2,7 @@ import image from '../assets/images/factory-inside.jpg'
 import Certified from '../components/Certified';
 import OurCompanies from '../components/OurCompanies';
 import Cover from './Shared/Cover';
+import ImageCardTimelineSlider from "../components/ImageCardTimelineSlider.jsx"
 
 const AboutCotton = () => {
     return (
@@ -9,6 +10,7 @@ const AboutCotton = () => {
            <Cover image={image} heading={"Our Story"}></Cover>
            <Certified></Certified>
            <OurCompanies></OurCompanies>
+           <ImageCardTimelineSlider></ImageCardTimelineSlider>
         </div>
     );
 };
