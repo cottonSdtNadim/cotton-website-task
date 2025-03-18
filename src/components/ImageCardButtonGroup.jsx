22a1cbcb1d19863bc2a8458c26b0b2772c54a516
cottonSdtNadim <nadim.mahmud@cottongroup.com.bd>
2025-03-18 11:17:39 +0600

    Section Title added

--- src/components/ImageCardButtonGroup.jsx
+++ src/components/ImageCardButtonGroup.jsx
@@ -1,14 +1,17 @@
 import SectionTitle from "./SectionTitle";
 
-
 const ImageCardButtonGroup = () => {
-    return (
-        <div>
-            <SectionTitle 
-            title={"Our Core values"}
-            ></SectionTitle>
-        </div>
-    );
+  return (
+    <div>
+      <SectionTitle
+        title={"Our Core values"}
+        iconTop="-30px"
+        iconLeft="465px"
+        iconWidth="w-8"
+        iconHeight="h-12"
+      ></SectionTitle>
+    </div>
+  );
 };
 
 export default ImageCardButtonGroup;
